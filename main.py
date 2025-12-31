4c453e50dc170c40a7b3a144c9b31a5481617d10
Mar-ooq <marcel.blazejowski.dev@gmail.com>
2025-12-31 14:01:45 +0100

    feat: checking if config file exists and creating one if doesn't exist

---
+++ main.py
@@ -0,0 +1,5 @@
+import tkinter
+import os
+
+def setup():
+    
---
+++ setup.py
@@ -0,0 +1,19 @@
+import configparser
+import os
+
+
+def create_config():
+    config = configparser.ConfigParser()
+    config['URL'] = {'file_path' : ""}
+    config['exceptions'] = {}
+
+    with open('config.conf','w') as config_file:
+        config.write(config_file)
+
+
+
+config_file_path = "./config.conf"
+check_if_config_file_exists = os.path.exists(config_file_path)
+
+if check_if_config_file_exists is False:
+    create_config()
