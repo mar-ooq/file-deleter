374efa5ee02e52cd559038fb19dbcee7da5719cf
Mar-ooq <marcel.blazejowski.dev@gmail.com>
2026-01-01 22:47:21 +0100

    feat: setting max and min size of a window

--- main.py
+++ main.py
@@ -4,8 +4,10 @@ import os
 
 root = Tk(screenName="File Deleter", baseName=None,className='Tk', useTk=True, sync = False, use=None)
 
+#setting the size of a window
 root.geometry("400x400")
 root.minsize(300,300)
 root.maxsize(600,600)
+
 
 root.mainloop()
