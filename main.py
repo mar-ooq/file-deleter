e567fdba7d3cc54ea10f34342812e8938c49f7b9
Mar-ooq <marcel.blazejowski.dev@gmail.com>
2026-01-01 22:38:29 +0100

    chore: delete unnecessary file (test.py)

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import os
 root = Tk(screenName="File Deleter", baseName=None,className='Tk', useTk=True, sync = False, use=None)
 
 root.geometry("400x400")
-root.minsize()
-root.maxsize()
+root.minsize(300,300)
+root.maxsize(600,600)
 
 root.mainloop()
--- test.py
+++
@@ -1,5 +0,0 @@
-from tkinter import *
-from tkinter import ttk
-root = Tk()
-
-root.mainloop()
