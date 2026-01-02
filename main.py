a1ceee2bceddbaa967e144f2dcecf3e31e2da22c
Mar-ooq <marcel.blazejowski.dev@gmail.com>
2026-01-02 09:47:22 +0100

    feat: add labels, entry and button widgets

--- main.py
+++ main.py
@@ -9,5 +9,24 @@ root.geometry("400x400")
 root.minsize(300,300)
 root.maxsize(600,600)
 
+#Create a label
+label = Label(root, text = "Directory path", font =("Arial", 14))
 
+#Create an Entry widget
+entry = Entry(root, font=("Arial",14), width=30)
+
+#Create button
+enter_path_btn = Button(root, font=("Arial", 14), width=15,height=1,anchor="center", text = "Enter")
+
+#2nd label
+label2 = Label(root, text = "File extensions to exclude: ", font =("Arial", 14))
+
+#listing files
+directory_path = entry.get()
+files = os.listdir(directory_path)
+
+
+label.pack()
+entry.pack()
+enter_path_btn.pack()
 root.mainloop()
